ea93432f1a6fd1ad161d80a69a3c7a6c989c4984
Kavia AI <production@kavia.ai>
2025-10-29 07:23:27 +0000

    CheckPoint - cmd65a71d7

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -28,7 +29,7 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      // Handle images and fonts with asset modules
+      // Handle images and fonts with asset modules (from src imports)
       {
         test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
         type: 'asset/resource',
@@ -41,11 +42,27 @@ module.exports = {
   },
 
   plugins: [
+    // Copy all assets from public except index.html (handled by HtmlWebpackPlugin)
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public'),
+          to: path.resolve(__dirname, 'build'),
+          globOptions: {
+            ignore: ['**/index.html'], // let HtmlWebpackPlugin create index
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+
     // Generate the HTML from template
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
       filename: 'index.html',
       inject: 'body',
+      // Provide favicon to inject into HTML head
+      favicon: path.resolve(__dirname, 'public', 'favicon.ico'),
     }),
   ],
 
